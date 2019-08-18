8e21025074ec06ae4bb8d84cd30da9fceb9c8d9a
th3suit <mainframemadness@gmail.com>
2019-08-18 13:49:20 -0700

    Update registry_checker.ps1

--- registry_checker.ps1
+++ registry_checker.ps1
@@ -1,13 +1,13 @@
-#This script will READ registry locations from a CSV file ($origCSV), check their values, and create a new CSV ($resultsCSV) with the current values in a new column.
-#-The original CSV is not modified.
+# This script will READ registry locations from a CSV file ($origCSV), check their values, and create a new CSV ($resultsCSV) with the current values in a new column.
+# The original CSV is not modified.
 
-#DO THIS:
-#-Modify the variable $origCSV, with your CSV file location.
-#-Modify the variable $resultsCSV, to name the results file.
-#-The CSV must have the following column names: RegPath, Key, ExpectedValue
+# DO THIS:
+#  -Modify the variable $origCSV, with your CSV file location.
+#  -Modify the variable $resultsCSV, to name the results file.
+#  -The CSV must have the following column names: RegPath, Key, ExpectedValue
 
-#For RegPath, you must use the path found in regedit.
-#-For the root of keys, make sure to use the shortened version (HKLM, HKCU, etc.)
+# For RegPath, you must use the path found in regedit.
+#  -For the root of keys, make sure to use the shortened version (HKLM, HKCU, etc.)
 
 Clear-Host
 
