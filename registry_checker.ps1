613489473673cafb491009138a367e7a702188dc
th3suit <mainframemadness@gmail.com>
2020-07-04 20:04:24 -0700

    Update registry_checker.ps1
    1. Clarified process.
    2. Clarified instructions.
    3. Added comments in code.

--- registry_checker.ps1
+++ registry_checker.ps1
@@ -1,25 +1,28 @@
-# Process:
+# PROCESS:
 #  1. READ registry locations from a CSV file ($origCSV)
-#  2. Grab their values
+#  2. Retrieve configured values from the system.
 #  3. Write current values into a new column in a new file ($resultsCSV)
 
-# DO THIS:
-#  -Modify the variable $origCSV, with your CSV file location.
-#  -Modify the variable $resultsCSV, to name the results file.
-#  -The CSV must have the following column names: RegPath, Key, ExpectedValue
-
-# For RegPath, you must use the path found in regedit.
-#  -For the root of keys, make sure to use the shortened version (HKLM, HKCU, etc.)
+# INSTRUCTIONS:
+#  1. Modify the variable $origCSV, with your CSV file location.
+#    a. The CSV must have the following column names in cells listed: A1=RegPath, B1=Key, C1=ExpectedValue
+#    b. For RegPath value, you must use the path found in regedit.
+#    c. For the keys, make sure to use the shortened version (HKLM, HKCU, etc.)
+#  2. Modify the variable $resultsCSV, to desired results filename.
+#  3. Run script.
 
 Clear-Host
 
 $origCSV = ".\registry_check.csv"
 $resultsCSV = ".\registry_check_results.csv"
 
+# Copy $origCSV and name the file $resultsCSV.
 Copy-Item -Path $origCSV -Destination $resultsCSV
 
 $temp = Import-Csv $resultsCSV
 
+# Go through each line of $temp, check the registry location, and then record the key value.
 ForEach ($line in $temp) { $line.CurrentValue = Get-ItemProperty $line.RegPath -Name $line.Key | findstr $line.Key }
 
+# Write all registry values into the $resultsCSV file.
 $temp | Export-Csv $resultsCSV -NoTypeInformation
